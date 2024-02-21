d9f5af15d74549a70c297503db1fbe04593da177
Tortue <Le_dragon_d@hotmail.com>
2024-02-21 10:46:22 +0100

    Update main.py

--- scripts/main.py
+++ scripts/main.py
@@ -116,7 +116,7 @@ def change_seed_in_seed_area():
 
 # Buttons
 async def plus(event):
-    print(event.currentTarget.className)
+    print(event.currentTarget)
     #indexDict.update({str(shape): await index_change_operation(dictionary[str(shape)], indexDict[str(shape)], 1)})
     #await after_index_change(str(shape))
 
